1ee6d4db1c52a57f97b91239e4c10c798c38dbdb
mugomes <murilogomes5792@gmail.com>
2026-04-07 01:17:47 -0300

    Added files project

---
+++ README.md
@@ -0,0 +1,29 @@
+# MGStateCity
+Obtém os estados e cidades via API do IBGE.
+
+---
+
+## 💙 Apoie
+
+- GitHub: https://github.com/sponsors/mugomes
+- More: https://mugomes.github.io/apoie.html
+
+## 👤 Autor
+
+**Murilo Gomes Julio**
+
+🔗 [https://www.mugomes.com.br](https://www.mugomes.com.br)
+
+📺 [https://youtube.com/@mugomesoficial](https://youtube.com/@mugomesoficial)
+
+---
+
+## License
+
+The MGStateCity is provided under:
+
+[SPDX-License-Identifier: LGPL-2.1-only](https://github.com/mugomes/mgstatecity/blob/main/LICENSE)
+
+Beign under the terms of the GNU Lesser General Public License version 2.1 only.
+
+All contributions to the MGStateCity are subject to this license.
---
+++ src/MGStateCity.php
@@ -0,0 +1,139 @@
+<?php
+// Copyright (C) 2026 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Site: https://mugomes.github.io
+
+namespace MGStateCity;
+
+class MGStateCity
+{
+    private string $cacheDir = '';
+    private int $cacheTTL = 86400; // 24h
+
+    public function setCacheDir(string $path, int $permission = 0777)
+    {
+        if (!is_dir($path)) {
+            mkdir($path, $permission, true);
+        }
+
+        $this->cacheDir = $path;
+    }
+
+    private function getCache(string $key, int $ttl): ?array
+    {
+        $file = $this->cacheDir . '/' . md5($key) . '.json';
+
+        if (!file_exists($file)) {
+            return null;
+        }
+
+        if (time() - filemtime($file) > $ttl) {
+            unlink($file);
+            return null;
+        }
+
+        return json_decode(file_get_contents($file), true);
+    }
+
+    public function setCache(string $key, array $data)
+    {
+        $file = $this->cacheDir . '/' . md5($key) . '.json';
+        file_put_contents($file, json_encode($data));
+    }
+
+    function getHTTPJSON(string $url): array
+    {
+        $cached = $this->getCache($url, $this->cacheTTL);
+        if ($cached !== null) {
+            return [$cached, null];
+        }
+
+        $response = @file_get_contents($url);
+
+        if ($response === false) {
+            return [null, 'Erro ao acessar API'];
+        }
+
+        $data = json_decode($response, true);
+
+        if ($data === null) {
+            return [null, 'Erro ao decodificar JSON'];
+        }
+
+        $this->setCache($url, $data);
+
+        return [$data, null];
+    }
+
+    public function listarEstados(): array
+    {
+        $url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+        list($data, $erro) = $this->getHTTPJSON($url);
+        if ($erro) return [null, $erro];
+
+        $estados = [];
+
+        foreach ($data as $row) {
+            if (!empty($row['nome']) && !empty($row['sigla'])) {
+                $estados[] = $row['sigla'] . ' - ' . $row['nome'];
+            }
+        }
+
+        return [$estados, null];
+    }
+
+    public function listMunicipios(string $uf):array
+    {
+        $url = sprintf('https://servicodados.ibge.gov.br/api/v1/localidades/estados/%s/municipios', urlencode($uf));
+
+        list($data, $erro) = $this->getHTTPJSON($url);
+        if ($erro) return [null, $erro];
+
+        $municipios = [];
+
+        foreach ($data as $item) {
+            if (isset($item['nome'])) {
+                $municipios[] = $item['nome'];
+            }
+        }
+
+        return [$municipios, null];
+    }
+
+    function buscarPorNome(string $nome, string $uf):array
+    {
+        $url = sprintf('https://servicodados.ibge.gov.br/api/v1/localidades/estados/%s/municipios', urlencode($uf));
+
+        list($data, $erro) =  $this->getHTTPJSON($url);
+        if ($erro) return [null, null, $erro];
+
+        $municipioID = 0;
+
+        foreach ($data as $m) {
+            if (strcasecmp($m['nome'], $nome) === 0) {
+                $municipioID = $m['id'];
+                break;
+            }
+        }
+
+        if ($municipioID === 0) {
+            return [null, null, "Município {$nome}/{$uf} não encontrado"];
+        }
+
+        $coordsURL = sprintf('https://servicodados.ibge.gov.br/api/v3/malhas/municipios/%d/metadados', $municipioID);
+
+        list($coordsData, $erro) = $this->getHTTPJSON($coordsURL, 604800); // 7 dias
+        if ($erro) return [null, null, $erro];
+
+        if (empty($coordsData)) {
+            return [null, null, "Coordenadas não encontradas para o ID {$municipioID}"];
+        }
+
+        $lat = strval($coordsData[0]['centroide']['latitude']);
+        $lon = strval($coordsData[0]['centroide']['longitude']);
+
+        return [$lat, $lon, null];
+    }
+}
